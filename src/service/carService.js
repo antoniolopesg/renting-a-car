fe0f0aee1966b522f9f3d44dc3f0af584805dd02
Antonio Lopes <antoniolopesg@protonmail.com>
2022-10-12 22:29:03 -0300

    feat: implements calculateFinalPrice in carService

--- src/service/carService.js
+++ src/service/carService.js
@@ -1,8 +1,14 @@
+const Tax = require('../entities/Tax')
 const BaseRepository = require('../repository/base/baseRepository')
 
 class CarService {
   constructor({ cars }) {
     this.carRepository = new BaseRepository({ file: cars })
+    this.taxesBasedOnAge = Tax.taxesBasedOnAge
+    this.currencyFormat = new Intl.NumberFormat('pt-BR', {
+      currency: 'BRL',
+      style: 'currency'
+    })
   }
   
   async getAvailableCar(carCategory) {
@@ -22,6 +28,16 @@ class CarService {
     const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds)
     const carId = carCategory.carIds[randomCarIndex]
     return carId
+  }
+
+  calculateFinalPrice(customer, carCategory, numberOfDays) {
+    const { age } = customer
+    const price = carCategory.price
+    const { then: tax } = this.taxesBasedOnAge
+      .find(tax => age >= tax.from && age <= tax.to)
+    const finalPrice =  tax * price * numberOfDays
+    const formattedPrice = this.currencyFormat.format(finalPrice)
+    return formattedPrice
   }
 }
 
